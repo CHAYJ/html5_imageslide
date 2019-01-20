cd9a72d85518c04e796746ad6b013e78eb0280c8
cheongbosik <cjsk1126@hanmail.net>
2019-01-20 16:07:34 +0900

    type2 method chainning added

--- HT502-imageSlider_template/js/type2.js
+++ HT502-imageSlider_template/js/type2.js
@@ -6,9 +6,10 @@ $(document).ready(function() {
   //$("#slider ul li:nth-child(3)").hide();
 
    setInterval(function(){
-      $("#slider ul li:first-of-type").fadeOut();
-      $("#slider ul li:nth-child(2)").fadeIn(); 
-      $("#slider ul").append($("#slider ul li:first-of-type"));
+      $("#slider ul li:first-of-type").fadeOut(2000).next().fadeIn(1000).end().appendTo($("#slider ul"));
+      // $("#slider ul li:first-of-type").fadeOut();
+      // $("#slider ul li:nth-child(2)").fadeIn(); 
+      // $("#slider ul").append($("#slider ul li:first-of-type"));
       
    }, 2000);
 });
